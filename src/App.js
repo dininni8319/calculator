68cf8e0ccb98dc7f5b821e7f8a57200e7fe129f9
Salvatore Dininni <s.dininni@yahoo.com>
2022-07-19 18:31:56 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { usestate } from 'react';
 
 function App() {
+  // const [ total, setTotal ] = usestate(0);
+  function general(val1, val2, action ) {
+    let total = 0
+    
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className="row custom-row">
+            <header className='custom-header'>
+
+            </header>
+            <main className="custom-main">
+                  <div className="custom-cell">
+                    <button>Clear</button>
+                    <button>/</button>
+                    <button>*</button>
+                    <button>=</button>
+                  </div>
+                  <div className="custom-cell">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                  </div>
+                  <div className="custom-cell">
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>-</button>
+                  </div>
+                  <div className="custom-cell">
+                    <button>8</button>
+                    <button>9</button>
+                    <button>0</button>
+                    <button>+</button>
+                  </div>
+            </main>
+       </div>
     </div>
   );
 }
---
+++ src/utilities.js
@@ -0,0 +1,15 @@
+export function multiply(n1, n2, total) {
+  return 
+}
+
+export function subtract(n1, n2, total) {
+  return
+}
+
+export function add(n1, n2, total) {
+  return
+}
+
+export function divide(params) {
+  return 
+}
