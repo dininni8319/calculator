6428ec5c210735d5c57174c987b73c3eedcfa490
Salvatore Dininni <s.dininni@yahoo.com>
2022-07-19 21:05:15 +0200

    react calculator

--- src/App.js
+++ src/App.js
@@ -1,45 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
-import { usestate } from 'react';
+import { useState } from 'react';
 
 function App() {
-  // const [ total, setTotal ] = usestate(0);
-  function general(val1, val2, action ) {
-    let total = 0
-    
+  const [ calc, setCalc ] = useState("");
+  const [ result, setResult ] = useState("");
+  
+  const operator = [ '/', '*', '+', '-', '.'];
+  
+  const handleResult = () => {
+    setCalc('');
+    setResult(eval(calc).toString())
   }
+
+  const handleDelete = () => {
+    if (calc == '') {
+       return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+    setResult(value);
+  }
+
+  const updateCalc = ( value ) => {
+    if (
+      operator.includes(value) && calc === '' ||
+      operator.includes(value) && operator.includes(calc.slice(-1))
+    
+    ) {
+      return;
+    }
+
+    if (!operator.includes(value)) {
+       setResult(eval(calc + value).toString())
+    }
+        setCalc( calc + value);
+  }
+  
+  const createDigits = () => {
+    let digits = [];
+
+    for (let i = 0; i <= 9; i++) {
+      
+      digits = [...digits,
+      <button 
+        key={i} 
+        onClick={() => updateCalc(i.toString())}
+      >{i}
+
+      </button>
+      ];
+    }
+
+    return digits;
+  }
+ 
   return (
     <div className="App">
-       <div className="row custom-row">
-            <header className='custom-header'>
+       <div className="calculator">
 
-            </header>
-            <main className="custom-main">
-                  <div className="custom-cell">
-                    <button>Clear</button>
-                    <button>/</button>
-                    <button>*</button>
-                    <button>=</button>
-                  </div>
-                  <div className="custom-cell">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                  </div>
-                  <div className="custom-cell">
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>-</button>
-                  </div>
-                  <div className="custom-cell">
-                    <button>8</button>
-                    <button>9</button>
-                    <button>0</button>
-                    <button>+</button>
-                  </div>
-            </main>
+            <div className='display'>
+             { result  ? <span className='results'>{result}</span> : ''} { calc || result || 0}
+            </div>
+
+            <div className="operators">
+              <button onClick={handleDelete}>DEL</button>
+              <button onClick={() => updateCalc('/')}>/</button>
+              <button onClick={() => updateCalc('*')}>*</button>
+              <button onClick={() => updateCalc('+')}>+</button>
+              <button onClick={() => updateCalc('-')}>-</button>
+            </div>
+            <div className="digitis"> 
+              { createDigits() }
+              <button onClick={handleResult}>=</button>
+              <button onClick={() => updateCalc('.')}>.</button>
+            </div>
        </div>
     </div>
   );
